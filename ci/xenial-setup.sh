d18ae5819f257a89b487c97f8fc7c27b485376c6
Simon Li <orpheus+devel@gmail.com>
2019-06-06 22:52:32 +0100

    Install Python 3.6 in Vagrantfile

--- ci/xenial-setup.sh
+++ ci/xenial-setup.sh
@@ -23,3 +23,8 @@ nameserver 8.8.4.4
 nameserver 8.8.8.8
 EOF
 sed -i -re "s/^(127.0.0.1\\s.+)/\\1 `hostname`/" /etc/hosts
+
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get update -q
+apt-get install -y -q python3.6 python3.6-gdbm python3.6-dev
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
