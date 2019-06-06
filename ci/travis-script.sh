273ed9151d330843b85d33688a737b78769f107c
Simon Li <orpheus+devel@gmail.com>
2019-06-06 23:32:46 +0100

    WIP: chartpress for publishing

---
+++ ci/travis-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eux
+
+python3 tools/templates/lint-and-validate.py
+# render & publish chart
+if [[
+    "$TRAVIS_BRANCH" == "master" &&
+    "$TRAVIS_PULL_REQUEST" == "false" &&
+    "$RUN_PUBLISH_SCRIPT" == "1"
+]]; then
+    ./ci/publish-chart.sh
+else
+    chartpress --commit-range ${TRAVIS_COMMIT_RANGE}
+fi
+git diff
+
+./ci/test.sh
