410cd2d84ece3bd719730827846f126ad10745f7
Simon Li <spli@dundee.ac.uk>
2019-06-07 18:47:47 +0100

    Basic functional test of OMERO
    Import image, get image in web.

--- ci/test.sh
+++ ci/test.sh
@@ -17,6 +17,24 @@ kubectl_retry() {
   }
 }
 
+display_logs() {
+  fold_start logs.1 "Display kubernetes logs"
+  # May crash on Travis:
+  #echo "***** minikube *****"
+  #minikube logs
+  echo "***** node *****"
+  kubectl_retry describe node
+  echo "***** pods *****"
+  kubectl_retry --namespace $TEST_NAMESPACE get pods
+  echo "***** events *****"
+  kubectl_retry --namespace $TEST_NAMESPACE get events
+  echo "***** hub *****"
+  kubectl_retry --namespace $TEST_NAMESPACE logs statefulset/omero-server
+  echo "***** proxy *****"
+  kubectl_retry --namespace $TEST_NAMESPACE logs deploy/omero-web
+  fold_end logs.1
+}
+
 set -eux
 
 # Is there a standard interface name?
@@ -43,6 +61,8 @@ helm install --name omero-server --namespace $TEST_NAMESPACE ./omero-server/ \
 helm install --name omero-web --namespace $TEST_NAMESPACE ./omero-web/ \
   -f minikube-omero-web.yaml $HELM_EXTRA_ARGS
 
+fold_start deploy.1 "Waiting for servers to be ready"
+
 echo "waiting for omero-server"
 n=0
 until [ "`kubectl_retry -n $TEST_NAMESPACE get statefulset omero-server -o jsonpath='{.status.readyReplicas}'`" = 1 ]; do
@@ -67,23 +87,14 @@ until [ "`kubectl_retry -n $TEST_NAMESPACE get deploy omero-web -o jsonpath='{.s
   sleep 10
 done
 
-display_logs() {
-  fold_start logs.1 "Display kubernetes logs"
-  # May crash on Travis:
-  #echo "***** minikube *****"
-  #minikube logs
-  echo "***** node *****"
-  kubectl_retry describe node
-  echo "***** pods *****"
-  kubectl_retry --namespace $TEST_NAMESPACE get pods
-  echo "***** events *****"
-  kubectl_retry --namespace $TEST_NAMESPACE get events
-  echo "***** hub *****"
-  kubectl_retry --namespace $TEST_NAMESPACE logs statefulset/omero-server
-  echo "***** proxy *****"
-  kubectl_retry --namespace $TEST_NAMESPACE logs deploy/omero-web
-  fold_end logs.1
-}
+fold_end deploy.1
+
+echo "Importing image"
+OMERO_PORT=$(kubectl -n $TEST_NAMESPACE get svc omero-server -o jsonpath='{.spec.ports[0].nodePort}')
+IMAGE_ID=$(docker run --rm -v $PWD:/data:ro --entrypoint /opt/omero/server/OMERO.server/bin/omero openmicroscopy/omero-server:5.4.10 \
+  import -s root@$IP:$OMERO_PORT -w omero -T Dataset:name:test /data/ci/opengraph-repo-image.jpg --encrypted true)
+
+pytest ci/test_image.py
 
 display_logs
 kubectl_retry --namespace $TEST_NAMESPACE get pods
