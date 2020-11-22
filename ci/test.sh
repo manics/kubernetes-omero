f8a29c82d6ee01a56693ece496c89fdd945494c4
Simon Li <spli@dundee.ac.uk>
2020-11-22 22:50:28 +0000

    pre-commit: beautysh

--- ci/test.sh
+++ ci/test.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 
 fold_start() {
-  echo "::group::$1"
+    echo "::group::$1"
 }
 
 fold_end() {
-  echo "::endgroup::"
+    echo "::endgroup::"
 }
 
 # kubectl seems to frequently loose its connection on Travis, auto-retry once
 kubectl_retry() {
-  kubectl "$@" || {
-    >&2 echo "kubectl failed, retrying..."
-    sleep 3
-    kubectl "$@"
-  }
+    kubectl "$@" || {
+        >&2 echo "kubectl failed, retrying..."
+        sleep 3
+        kubectl "$@"
+    }
 }
 
 display_logs() {
-  fold_start "Display kubernetes logs"
-  # May crash on Travis:
-  #echo "***** minikube *****"
-  #minikube logs
-  echo "***** node *****"
-  kubectl_retry describe node
-  echo "***** pods *****"
-  kubectl_retry --namespace $TEST_NAMESPACE get pods
-  echo "***** events *****"
-  kubectl_retry --namespace $TEST_NAMESPACE get events
-  echo "***** hub *****"
-  kubectl_retry --namespace $TEST_NAMESPACE logs statefulset/omero-server
-  echo "***** proxy *****"
-  kubectl_retry --namespace $TEST_NAMESPACE logs deploy/omero-web
-  fold_end
+    fold_start "Display kubernetes logs"
+    # May crash on Travis:
+    #echo "***** minikube *****"
+    #minikube logs
+    echo "***** node *****"
+    kubectl_retry describe node
+    echo "***** pods *****"
+    kubectl_retry --namespace $TEST_NAMESPACE get pods
+    echo "***** events *****"
+    kubectl_retry --namespace $TEST_NAMESPACE get events
+    echo "***** hub *****"
+    kubectl_retry --namespace $TEST_NAMESPACE logs statefulset/omero-server
+    echo "***** proxy *****"
+    kubectl_retry --namespace $TEST_NAMESPACE logs deploy/omero-web
+    fold_end
 }
 
 set -eux
 
 # Is there a standard interface name?
 for iface in eth0 ens4 enp0s3; do
-  IP=$(ip -o -4 addr show $iface | awk '{print $4}' | cut -d/ -f1);
-  if [ -n "$IP" ]; then
-    echo "IP: $IP"
-    break
-  fi
+    IP=$(ip -o -4 addr show $iface | awk '{print $4}' | cut -d/ -f1);
+    if [ -n "$IP" ]; then
+        echo "IP: $IP"
+        break
+    fi
 done
 if [ -z "$IP" ]; then
-  echo "Failed to get IP, current interfaces:"
-  ifconfig -a
-  exit 2
+    echo "Failed to get IP, current interfaces:"
+    ifconfig -a
+    exit 2
 fi
 
 TEST_NAMESPACE=omero-test
@@ -57,33 +57,33 @@ helm dependency update ./omero-server/
 helm dependency update ./omero-web/
 
 helm upgrade --install omero-server --namespace $TEST_NAMESPACE --create-namespace \
-  ./omero-server/ -f test-omero-server.yaml
+    ./omero-server/ -f test-omero-server.yaml
 helm upgrade --install omero-web --namespace $TEST_NAMESPACE --create-namespace \
-  ./omero-web/ -f test-omero-web.yaml
+    ./omero-web/ -f test-omero-web.yaml
 
 fold_start "waiting for omero-server"
 n=0
 until [ "`kubectl_retry -n $TEST_NAMESPACE get statefulset omero-server -o jsonpath='{.status.readyReplicas}'`" = 1 ]; do
-  let ++n
-  if [ $(( $n % 12 )) -eq 0 ]; then
-    kubectl_retry -n $TEST_NAMESPACE describe pod
-  else
-    kubectl_retry -n $TEST_NAMESPACE get pod
-  fi
-  sleep 10
+    let ++n
+    if [ $(( $n % 12 )) -eq 0 ]; then
+        kubectl_retry -n $TEST_NAMESPACE describe pod
+    else
+        kubectl_retry -n $TEST_NAMESPACE get pod
+    fi
+    sleep 10
 done
 fold_end
 
 fold_start "waiting for omero-web"
 n=0
 until [ "`kubectl_retry -n $TEST_NAMESPACE get deploy omero-web -o jsonpath='{.status.readyReplicas}'`" = 1 ]; do
-  let ++n
-  if [ $(( $n % 12 )) -eq 0 ]; then
-    kubectl_retry -n $TEST_NAMESPACE describe pod
-  else
-    kubectl_retry -n $TEST_NAMESPACE get pod
-  fi
-  sleep 10
+    let ++n
+    if [ $(( $n % 12 )) -eq 0 ]; then
+        kubectl_retry -n $TEST_NAMESPACE describe pod
+    else
+        kubectl_retry -n $TEST_NAMESPACE get pod
+    fi
+    sleep 10
 done
 fold_end
 
