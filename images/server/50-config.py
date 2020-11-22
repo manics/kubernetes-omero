1f948262370016af9500af21e0b6670aa0f1f518
Simon Li <spli@dundee.ac.uk>
2020-11-22 22:49:33 +0000

    pre-commit: black

--- images/server/50-config.py
+++ images/server/50-config.py
@@ -9,16 +9,16 @@ from subprocess import call
 from re import sub
 
 
-OMERO = '/opt/omero/server/venv3/bin/omero'
+OMERO = "/opt/omero/server/venv3/bin/omero"
 
-rc = call([OMERO, 'load', '--glob', '/opt/omero/server/config/*.omero'])
+rc = call([OMERO, "load", "--glob", "/opt/omero/server/config/*.omero"])
 assert rc == 0
 
 for (k, v) in os.environ.items():
-    if k.startswith('CONFIG_'):
+    if k.startswith("CONFIG_"):
         prop = k[7:]
-        prop = sub('([^_])_([^_])', r'\1.\2', prop)
-        prop = sub('__', '_', prop)
+        prop = sub("([^_])_([^_])", r"\1.\2", prop)
+        prop = sub("__", "_", prop)
         value = v
-        rc = call([OMERO, 'config', 'set', '--', prop, value])
+        rc = call([OMERO, "config", "set", "--", prop, value])
         assert rc == 0
