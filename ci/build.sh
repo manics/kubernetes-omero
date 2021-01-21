c9f04b5ddb8d1a11b900bcfc5e7c040cdc32cd00
Simon Li <spli@dundee.ac.uk>
2021-01-21 22:22:08 +0000

    investigate github workflow

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -eux
 
+env
+
 helm lint omero-server omero-web
 
-./ci/chartpress.py
+/usr/share/miniconda/bin/pip list
+which python
+which conda
+python -c 'import ruamel.yaml'
+python -c 'from ruamel.yaml import YAML'
+/usr/share/miniconda/bin/python ./ci/chartpress.py
 git diff
 
 ./ci/test.sh
